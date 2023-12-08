1b1b04cdc64b8188ac4e3497a0893a5203d53109
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2023-12-09 03:19:40 +0900

    refactor: 不要なルーティングやコマンド、チャンネルを削除 #8

--- routes/api.php
+++ routes/api.php
@@ -14,10 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
 Route::prefix('/v1')->name('v1.')->group(static function () {
     Route::prefix('notification')->name('notification.')->group(static function () {
         // メール送信
